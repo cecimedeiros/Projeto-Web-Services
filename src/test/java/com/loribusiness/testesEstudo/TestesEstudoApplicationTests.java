1443918328870edc6e61cb4965e5912c16410e22
cecimedeiros <cecilialmedeirosc@gmail.com>
2024-05-12 12:54:18 -0300

    Projeto criado

---
+++ src/test/java/com/loribusiness/testesEstudo/TestesEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loribusiness.testesEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
