0fbe122a60337e498c5b7deb1bdd14d91021258f
cecimedeiros <cecilialmedeirosc@gmail.com>
2024-05-17 22:21:53 -0300

    criação das classes OrderItem (para auxiliar na questão de um pedido ter mais de um item) e OrderItemPK (para ser a chave desse OrderItem)

---
+++ src/main/java/com/loribusiness/testesEstudo/entities/OrderItem.java
@@ -0,0 +1,79 @@
+package com.loribusiness.testesEstudo.entities;
+
+import com.loribusiness.testesEstudo.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    //isso pq um pedido tem itens mas tambem tem uma quantidade de itens, essa classe serve para ser isso.
+
+    @EmbeddedId //vamos ser nós não a db que vai criar
+    private OrderItemPK id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        //o id aqui ta sendo criado no grosso, decompondo os atributos que o compõe para criar ele aqui
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    //get e set de id não existe, existe dos atrinutos dele, dessa forma:
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
